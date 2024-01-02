04b35cf60fd2bcd807af42ecc18caca4f8b9d4f7
chulinx <chulinx@163.com>
2024-01-02 19:22:11 +0800

    [optimize] optimize template format

--- pkg/nginx/templates.go
+++ pkg/nginx/templates.go
@@ -6,11 +6,11 @@ var (
           listen       {{.ListenPort}};
           server_name {{.ServerName}};
           {{ range $Proxy := .Proxys }}location / {
-          {{ if $Proxy.NameSpace }}proxy_pass {{$Proxy.Scheme}}://{{$Proxy.Service}}.{{$Proxy.NameSpace}}:{{$Proxy.Port}};{{ else }}proxy_pass {{$Proxy.Scheme}}://{{$Proxy.Service}}:{{$Proxy.Port}};{{ end }}
-          {{ if not $Proxy.ProxyRedirect }}proxy_redirect     off;{{ end }}
-          {{ if $Proxy.ProxyHttpVersion }}proxy_http_version {{ $Proxy.ProxyHttpVersion }};{{ end }}
-          {{ range $key,$value := $Proxy.ProxyHeaders }}proxy_set_header   {{ $key }} {{ $value }};
-          {{ end }}
+             {{ if $Proxy.NameSpace }}proxy_pass {{$Proxy.Scheme}}://{{$Proxy.Service}}.{{$Proxy.NameSpace}}:{{$Proxy.Port}};{{ else }}proxy_pass {{$Proxy.Scheme}}://{{$Proxy.Service}}:{{$Proxy.Port}};{{ end }}
+             {{ if not $Proxy.ProxyRedirect }}proxy_redirect     off;{{ end }}
+             {{ if $Proxy.ProxyHttpVersion }}proxy_http_version {{ $Proxy.ProxyHttpVersion }};{{ end }}
+             {{ range $key,$value := $Proxy.ProxyHeaders }}proxy_set_header   {{ $key }} {{ $value }};
+             {{ end }}
           }
 		  access_log    /var/log/nginx/access.log;{{ end }}
       }`
